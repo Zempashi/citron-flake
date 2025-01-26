13358219b098ff1b0c659d48fca5f3d07163d699
Julien Girardin <jugirardin@free.fr>
2025-01-26 14:12:29 +0100

    New sha for citron-master ?

--- citron-master.nix
+++ citron-master.nix
@@ -6,7 +6,7 @@ let
     deepClone = true;
     url = "https://git.citron-emu.org/Citron/Citron";
     rev = "be191f740a477290f6dae570fa615aaf0d24bdd4";
-    sha256 = "sha256-YGy3dOQM7wi+Qci+qK7RrDc3VkQO14k5tB5cJas/pAs=";
+    sha256 = "sha256-D/sPgn030kl3G96IHHYzG/KGjBhJdheyzLZKjSuxUcs=";
   };
 in
 pkgs.qt6.callPackage ./citron.nix ({ inherit nx_tzdb version src; })
