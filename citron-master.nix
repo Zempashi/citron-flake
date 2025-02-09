9f6cdc18758778bb31c4e026c03176d7f6e31b2c
Julien Girardin <jugirardin@free.fr>
2025-02-09 14:05:40 +0100

    Update citron-master with 2020-02-09

--- citron-master.nix
+++ citron-master.nix
@@ -1,12 +1,12 @@
 { pkgs, nx_tzdb, ... }:
 let
-  version = "unstable-2025-01-26";
+  version = "unstable-2025-02-09";
   src = pkgs.fetchgit {
     fetchSubmodules = true;
     deepClone = true;
     url = "https://git.citron-emu.org/Citron/Citron";
-    rev = "be191f740a477290f6dae570fa615aaf0d24bdd4";
-    sha256 = "sha256-D/sPgn030kl3G96IHHYzG/KGjBhJdheyzLZKjSuxUcs=";
+    rev = "13ada2d705137483f7004374438135d2a918e578";
+    sha256 = "sha256-yudrigcwOjWtQcdGUJdRtD5DcwbXMrPyaypsQCp4J9U=";
   };
 in
 pkgs.qt6.callPackage ./citron.nix ({ inherit nx_tzdb version src; })
